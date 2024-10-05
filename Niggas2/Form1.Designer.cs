d95ed0a3a4d7ab1eb8408fce310f13215eda59c5
Gabriel Norwin Lacayo Marenco <165950979+GlacayoMar@users.noreply.github.com>
2024-10-05 03:01:52 -0600

    en teoria ya se pueden guardar los archivs, no he hecho pruebas porque ando sueño, mas tarde sigo con el resto

---
+++ Niggas2/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Niggas2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.tbGrade = new System.Windows.Forms.TextBox();
+            this.tbSex = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvPersons = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Edad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Genero";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(204, 44);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 22);
+            this.tbName.TabIndex = 4;
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(204, 85);
+            this.tbAge.Name = "tbAge";
+            this.tbAge.Size = new System.Drawing.Size(100, 22);
+            this.tbAge.TabIndex = 5;
+            // 
+            // tbGrade
+            // 
+            this.tbGrade.Location = new System.Drawing.Point(204, 129);
+            this.tbGrade.Name = "tbGrade";
+            this.tbGrade.Size = new System.Drawing.Size(100, 22);
+            this.tbGrade.TabIndex = 6;
+            // 
+            // tbSex
+            // 
+            this.tbSex.Location = new System.Drawing.Point(204, 175);
+            this.tbSex.Name = "tbSex";
+            this.tbSex.Size = new System.Drawing.Size(100, 22);
+            this.tbSex.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvPersons);
+            this.groupBox1.Location = new System.Drawing.Point(103, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 203);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // dgvPersons
+            // 
+            this.dgvPersons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPersons.Location = new System.Drawing.Point(3, 18);
+            this.dgvPersons.Name = "dgvPersons";
+            this.dgvPersons.RowHeadersWidth = 51;
+            this.dgvPersons.RowTemplate.Height = 24;
+            this.dgvPersons.Size = new System.Drawing.Size(402, 182);
+            this.dgvPersons.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(364, 106);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 28);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(364, 140);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 24);
+            this.btnRead.TabIndex = 10;
+            this.btnRead.Text = "Leer";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(364, 171);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 29);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbSex);
+            this.Controls.Add(this.tbGrade);
+            this.Controls.Add(this.tbAge);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.TextBox tbGrade;
+        private System.Windows.Forms.TextBox tbSex;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvPersons;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Niggas2/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Niggas2.modells;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Niggas2
+{
+    public partial class Form1 : Form
+    {
+        private List<Persona> listapersonas = new List<Persona>();
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarDataGridView();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InicializarDataGridView() 
+        {
+            dgvPersons.Columns.Add("Nombre", "Nombre");
+            dgvPersons.Columns.Add("Edad", "Edad");
+            dgvPersons.Columns.Add("Nota", "Nota");
+            dgvPersons.Columns.Add("Genero", "Genero");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (listapersonas.Count < 3)
+            {
+                string nombre = tbName.Text;
+                int edad = int.Parse(tbAge.Text);
+                int nota = int.Parse(tbGrade.Text);
+                char genero = char.Parse(tbSex.Text);
+
+                Persona persona = new Persona(nombre, edad, nota, genero);
+                listapersonas.Add(persona);
+
+                dgvPersons.Rows.Add(persona.Nombre, persona.Edad, persona.Nota, persona.Genero);
+
+                LimpiarTextBoxes();
+            }
+            else 
+            {
+                MessageBox.Show("Solo se pueden agregar tres personas por archivo");
+            }
+
+        }
+
+        private void LimpiarTextBoxes()
+        {
+            tbName.Clear();
+            tbGrade.Clear();
+            tbAge.Clear();
+            tbSex.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (listapersonas.Count == 3)
+            {
+                SaveFileDialog save = new SaveFileDialog();//Stackoverflow
+                save.Filter = "Archivos binarios (*.dat)|*.dat";//Stackoverflow
+
+                if (save.ShowDialog() == DialogResult.OK) //stackoverflow
+                {
+                    using (FileStream nah /* lo escribi asi porque ando pereza*/ = new FileStream(save.FileName, FileMode.Create, FileAccess.Write)) //esto lo sauqe de un mae de stackoverflow, no encuentro la publicacion asi que ahi se queda
+                    using (BinaryWriter wea /* aqui tambien andaba pereza*/ = new BinaryWriter(nah))
+                    {
+                        foreach (Persona person in listapersonas)
+                        {
+                            wea.Write(person.Nombre.Length);
+                            wea.Write(person.Nombre.ToCharArray());
+                            wea.Write(person.Edad);
+                            wea.Write(person.Nota);
+                            wea.Write(person.Genero);
+                        }
+                    }
+
+                    MessageBox.Show("Archivo guardado correctamente.");
+                    listapersonas.Clear();
+                    dgvPersons.Rows.Clear();
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Debe agregar exactamente 3 personas antes de guardar.");
+            }
+        }
+    }
+}
